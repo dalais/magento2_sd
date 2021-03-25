40fbde4d0d41a273a0ef85379c06c96057a3b834
Dalay Salchak <okeanol777@gmail.com>
2021-03-25 18:51:57 +0700

    blog post comment module setup

---
+++ app/code/Magespace/PostComment/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Magespace_PostComment',
+        __DIR__
+    );

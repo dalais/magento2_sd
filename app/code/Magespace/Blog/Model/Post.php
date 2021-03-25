ecbe9010f01048849dae962c0060ea647e0524d3
Dalay Salchak <okeanol777@gmail.com>
2021-03-25 17:35:50 +0700

    fix post model implement

--- app/code/Magespace/Blog/Model/Post.php
+++ app/code/Magespace/Blog/Model/Post.php
@@ -2,28 +2,18 @@
 
 namespace Magespace\Blog\Model;
 
+use Magespace\Blog\Api\Data\PostInterface;
+
 /**
  * Class Post
  */
-class Post extends \Magento\Framework\Model\AbstractModel implements
-    \Magespace\Blog\Api\Data\PostInterface,
-    \Magento\Framework\DataObject\IdentityInterface
+class Post extends \Magento\Framework\Model\AbstractModel implements PostInterface
 {
-    const CACHE_TAG = 'magespace_blog_page_post';
-
     /**
      * Init
      */
     protected function _construct() // phpcs:ignore PSR2.Methods.MethodDeclaration
     {
         $this->_init(\Magespace\Blog\Model\ResourceModel\Post::class);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getIdentities()
-    {
-        return [self::CACHE_TAG . '_' . $this->getId()];
     }
 }

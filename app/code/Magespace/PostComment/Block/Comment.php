2e9ecca6d5a440dc7181b86d0a0fb94a1353beb5
Dalay Salchak <okeanol777@gmail.com>
2021-03-26 14:14:29 +0700

    comment section on page

---
+++ app/code/Magespace/PostComment/Block/Comment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Magespace\PostComment\Block;
+
+use Magespace\Blog\Api\PostRepositoryInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Cms\Model\Page;
+use Magespace\Blog\Model\Post;
+
+/**
+ * Class Main
+ */
+class Comment extends Template
+{
+    protected $cmsPage;
+
+    protected $postRepository;
+
+    public function __construct(
+        Context $context,
+        Page $cmsPage,
+        PostRepositoryInterface $postRepository
+    ) {
+        parent::__construct($context);
+        $this->cmsPage = $cmsPage;
+        $this->postRepository = $postRepository;
+    }
+
+    public function isPost()
+    {
+        /** @var Post $post */
+        $post = $this->postRepository->getByPageId($this->cmsPage->getId());
+        return !$post->isEmpty() && $post->getData('is_post') > 0;
+    }
+
+    public function getComments()
+    {
+        return 'Comments';
+    }
+}
